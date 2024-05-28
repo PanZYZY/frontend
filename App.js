9aed511c1e1cfdb44da00c75e961d8976c6010c0
PanZYZY <yammiamy@gmail.com>
2024-05-29 01:32:36 +1000

    Change screens

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
 import TasksScreen from './screens/TasksScreen';
 import CalendarScreen from './screens/CalendarScreen';
 import AboutScreen from './screens/AboutScreen';
 import SettingsScreen from './screens/SettingsScreen';
-import SplashScreen from './screens/SplashScreen';
-import LoginScreen from './screens/LoginScreen';
 
 const Tab = createBottomTabNavigator();
 
